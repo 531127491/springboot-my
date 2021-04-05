15b4bbf7e60ff3ffef2c0a86b87cd7109216a1cb
sjl <531127491@qq.com>
2021-04-05 13:47:22 +0800

    解决跨域问题

---
+++ src/main/java/com/winter/Configuration/WebMvcConfigurer.java
@@ -0,0 +1,17 @@
+package com.winter.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+
+    }
+}
+
